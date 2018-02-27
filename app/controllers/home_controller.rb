ba88f181a51b0776c8ae1cc9b299dd802feae774
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2018-02-27 17:16:20 -0600

    Add Websockets Channel for messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def welcome
-  	render html: "Welcome"
+  	render html: "<a href='/send'><p>Send a message</p></a>".html_safe
+  end
+
+  def send_message
+  	ActionCable.server.broadcast 'messages', {
+  		message: 'Sent from API using Action Cable - ' + Time.now.to_s,
+  		username: [ "Bob", "Mick", "Jon", "Diego" ].sample
+  	}
+
+  	redirect_to root_path
   end
 end
---
+++ lib/tasks/send_message.rake
@@ -0,0 +1,8 @@
+namespace :send do
+
+	task :message => :environment do
+		puts "This should send message to be broadcasted"
+
+	end
+
+end
